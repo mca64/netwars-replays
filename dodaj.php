9caa7ed902dbe04e96de3c529c46310dd239597e
mca64 <mca640@gmail.com>
2014-09-30 13:54:14 +0200

    Create dodaj.php

---
+++ dodaj.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>
+        <!-- TITLE -->
+    </title>
+    <!-- STYLE -->
+    <link rel="Stylesheet" type="text/css" href="style.css" />
+    <style type="text/css">
+    </style>
+    <!-- SCRIPT -->
+</head>
+<body>
+    <div id="strona">
+        <div id="logo">
+        </div>
+        <div id="main">
+            <!-- LOGIN -->
+            <br />
+            <!-- CONTENT -->
+            Yo bitches!
+            <br>
+            <br>
+            <form name="addRep" method="post" action="wyslanie.php" enctype="multipart/form-data">
+                Replay:
+                <input name="plik" type="file" />
+                <br>Nick:
+                <input name="nick" type="text" size="10" value="Glow`mca64" />
+                <br>Opis:
+                <textarea name="opis" rows=20 cols=100></textarea>
+                <br>
+                <input type="submit" value="Wyślij!" />
+            </form>
+        </div>
+        <div id="menu-wrapper">
+            <div id="gora-menu">
+            </div>
+            <div id="menu-content">
+                <a href="http://netwars.pl/forum/">Forum</a>
+                <br>
+                <br>
+                <a href="http://127.0.0.1/~nw/replays.php">Replays</a>
+            </div>
+            <!-- STREAMS -->
+        </div>
+    </div>
+    <div id="footer">
+        <!-- ONLINE -->
+    </div>
+</body>
+</html>
